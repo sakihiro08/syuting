43321713ee3ae687d88e13184a211a4d813e7cb6
YuukiSakihiro <k021g1259@g.neec.ac.jp>
2022-06-07 12:03:04 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //playerÇÃÉèÅ[ÉãÉh
+        Vector3 pos = transform.position;
+        //âE
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += 0.01f;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= 0.01f;
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.z += 0.01f;
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.z -= 0.01f;
+
+        }
+        transform.position = new Vector3(pos.x, pos.y, pos.z);
+    }
+}
